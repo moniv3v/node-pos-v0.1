9437124746a1243cb8a2ed55fd5ccd3472ae196c
Monique <moniquechan1220@gmail.com>
2018-11-20 00:03:18 +0800

    Completed

--- main/main.js
+++ main/main.js
@@ -7,33 +7,47 @@ module.exports = function printInventory(array) {
         resultArray.forEach(element2 => {
         if (element.Barcode == element2.Barcode) {
             element2.Number = element2.Number+1;
-            element2.Price += element.Price;
+            if(element.Unit=='bottle'){
+                element2.Unit = ' bottles';
+            }
             exist = true;
         }
         });
         if (!exist) {
-            resultArray.push({
-                Barcode: element.Barcode,
-                Number: 1,
-                Name: element.Name,
-                Unit: element.Unit,
-                Price: element.Price
-            });
+            if(element.Unit!='a'){
+                resultArray.push({
+                    Barcode: element.Barcode,
+                    Number: 1,
+                    Name: element.Name,
+                    Unit: element.Unit,
+                    Price: element.Price
+                });
+            }else{
+                resultArray.push({
+                    Barcode: element.Barcode,
+                    Number: 1,
+                    Name: element.Name,
+                    Unit: '',
+                    Price: element.Price
+                });
+            }
+            
         }
     });
 
+    
     resultStr = "***<store earning no money>Receipt ***\n";
     resultArray.forEach(element => {
-        resultStr += "Name: "+element.Name+", Quantity: "+element.Number+", Unit price: "+element.Price+".00("+element.Unit+"), Subtotal: "+element.Price*element.Number+".00 (yuan)\n";
+        resultStr += "Name: "+element.Name+", Quantity: "+element.Number+element.Unit+", Unit price: "+element.Price+".00 (yuan), Subtotal: "+element.Price*element.Number+".00 (yuan)\n";
     });
     resultStr+="----------------------\n";
     resultStr+="Total: ";
     resultArray.forEach(element => {
-        total+=element.Price;
+        total+=element.Price*element.Number;
     });
     resultStr+=total+".00 (yuan)\n";
-    resultStr+='**********************';
+    resultStr+='**********************\n';
     
-    console.log(resultStr);
+    console.log(resultArray);
     return resultStr;
 };
